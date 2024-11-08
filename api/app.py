f1b0eacae8ecdb95950c6906445318abe5b1dd23
Jan Timpe <jtimpe@goraft.tech>
2024-11-08 15:53:29 -0500

    add worker flask app

--- api/app.py
+++ api/app.py
@@ -1,10 +1,18 @@
+import os
+import requests
 from flask import Flask
 
 app = Flask(__name__)
 
+WORKER_URI = os.environ.get("WORKER_URI")
+
 @app.route("/")
 def hello():
     return "Hello, World!"
+
+@app.route("/dowork")
+def do_work():
+    return requests.get(f'{WORKER_URI}/hello').content
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
